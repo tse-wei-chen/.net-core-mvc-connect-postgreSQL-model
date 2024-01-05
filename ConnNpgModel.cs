d0df1331af79710af0d8cfeb80edca8b8e3756b4
tse-wei-chen <gary20020322@gmail.com>
2024-01-05 21:47:16 +0800

    ver1

---
+++ ConnNpgModel.cs
@@ -0,0 +1,38 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace test.Models
+{
+    public class ConnNpgModel
+    {
+        string cs = "Host=localhost;Username=postgres;Password=root8888;Database=restaurant";
+        NpgsqlConnection con;
+
+        public ConnNpgModel()
+        {
+            con = new NpgsqlConnection(cs);
+        }
+
+        public void ConnectToDB()
+        {
+            try
+            {
+                con.Open();
+                Console.WriteLine("Connected to PostgreSQL database!");
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+        public void CloseConnection()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+                Console.WriteLine("Connection closed.");
+            }
+        }
+    }
+}
